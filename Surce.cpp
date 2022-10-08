254002a75f6b590a7c8153d6c27d0f1fcdda5b63
zxcown <115359959+zxcown@users.noreply.github.com>
2022-10-08 18:59:11 +0300

    Add files via upload

---
+++ Surce.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//1
+	/*int hp;
+	cin >> hp;
+	if (hp > 0) {
+		cout << "true" << endl;
+	}
+	if (hp <0) {
+		cout << "false" << endl;
+	}*/
+	//2
+	/*int t;
+	cin >> t;
+	if (t >= 20) {
+		cout << ("включен") << endl;
+	}
+	if (t < 20) {
+		cout << ("выключен") << endl;
+	}*/
+	//3
+	/*int t, c,f;
+	cin >> t >> c>> f;
+	if (c > t && f <= 80){
+	cout << "on" << endl;
+	}
+	else if (c <= t || f > 80) {
+		cout << "off" << endl;
+	}*/
+	//4
+	/*double x , y;
+	cin >> x ;
+	if (x > 0) {
+		 y = sqrt(x - 2);
+		 cout << y << endl;
+	}
+	else if (x == 0) {
+		 y = 0;
+		 cout << y << endl;
+	}
+	else if (x < 0) {
+		 y = abs(x);
+		 cout << y << endl;	
+	}*/
+	//5
+	/*int fin;
+	cin >> fin;
+	switch (fin) {
+	case 1: cout << "большой" << endl; break;
+	case 2: cout << "указательный" << endl; break;
+	case 3: cout << "средний" << endl; break;
+	case 4: cout << "безымянный" << endl; break;
+	case 5: cout << "мезинец" << endl; break;
+	default:cout << "ошибка" << endl; break;
+	}*/
+
+	
+}
+
